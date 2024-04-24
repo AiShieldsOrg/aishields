b699630325db057ce4e9f48ebec49dffbc1aead7
Akerke Balgabekova <akerke.kass@gmail.com>
2024-04-24 16:57:32 -0700

    modified test cases to be more general, shuffled the order

--- tests/test_sensitive_data_sanitizer.py
+++ tests/test_sensitive_data_sanitizer.py
@@ -7,12 +7,12 @@ test_cases = {
     "EMAIL": "john.doe@example.com",
     "IBAN_CODE": "DE89370400440532013000",
     "IP_ADDRESS": "192.168.0.1",
-    "PERSON_NAME": "Akerke Kass",
     "PHONE_NUMBER": "555-123-4567",
     "US_SSN": "123-45-6789",
-    "US_BANK_ACCOUNT": "123456789",
     "UUID": "a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a11",
-    "US_DRIVING_LICENSE": "CA1234567"
+    "US_DRIVING_LICENSE": "CA1234567",
+    "PERSON_NAME": "John Doe",
+    "US_BANK_ACCOUNT": "123456789",
 }
 
 
@@ -26,6 +26,4 @@ def test_sanitize_input(sensitive_data_sanitizer):
         input_prompt = f"Test with {input_content}"
         placeholder = SENSITIVE_DATA_CONFIGS[entity]["placeholder"]
         sanitized_input = sensitive_data_sanitizer.sanitize_input(input_prompt)
-        assert placeholder in sanitized_input, f"Failed to sanitize {entity}"
-
-
+        assert placeholder in sanitized_input, f"Failed to sanitize {entity} in {input_prompt}"
