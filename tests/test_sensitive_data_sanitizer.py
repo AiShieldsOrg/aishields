7a700b7dbc173b0c63042901052fe51ca94b44e7
Akerke Balgabekova <akerke.kass@gmail.com>
2024-04-24 15:12:23 -0700

    added simple, initial test with 10 cases for config

---
+++ tests/test_sensitive_data_sanitizer.py
@@ -0,0 +1,31 @@
+import pytest
+from ai_sanitizer_app.sensitive_data_sanitizer import SensitiveDataSanitizer
+from config import SENSITIVE_DATA_CONFIGS
+
+test_cases = {
+    "CREDIT_CARD": "4012-8888-8888-8881",
+    "EMAIL": "john.doe@example.com",
+    "IBAN_CODE": "DE89370400440532013000",
+    "IP_ADDRESS": "192.168.0.1",
+    "PERSON_NAME": "Akerke Kass",
+    "PHONE_NUMBER": "555-123-4567",
+    "US_SSN": "123-45-6789",
+    "US_BANK_ACCOUNT": "123456789",
+    "UUID": "a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a11",
+    "US_DRIVING_LICENSE": "CA1234567"
+}
+
+
+@pytest.fixture
+def sensitive_data_sanitizer():
+    return SensitiveDataSanitizer()
+
+
+def test_sanitize_input(sensitive_data_sanitizer):
+    for entity, input_content in test_cases.items():
+        input_prompt = f"Test with {input_content}"
+        placeholder = SENSITIVE_DATA_CONFIGS[entity]["placeholder"]
+        sanitized_input = sensitive_data_sanitizer.sanitize_input(input_prompt)
+        assert placeholder in sanitized_input, f"Failed to sanitize {entity}"
+
+
